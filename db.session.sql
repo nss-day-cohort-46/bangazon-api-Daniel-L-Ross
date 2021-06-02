afff215be9ec743f5dc0b7edf660d1f6adccecc8
Dan Ross <daniellross@gmail.com>
2021-06-02 11:00:59 -0500

    reference the same table multiple times to achieve desired result

--- db.session.sql
+++ db.session.sql
@@ -1,8 +1,12 @@
 -- customer name header
 -- seller names as bulleted list
 
-SELECT *
-    -- first_name || ' ' || last_name as fullname
-FROM auth_user 
-JOIN bangazonapi_customer c ON c.user_id = auth_user.id
-JOIN bangazonapi_favorite f ON f.customer_id = c.id
+SELECT
+    c.id as customer_id,
+    a.first_name || ' ' || a.last_name as customer_name,
+    b.first_name || ' ' || b.last_name as seller_name
+FROM bangazonapi_favorite f
+JOIN bangazonapi_customer c ON c.id = f.customer_id
+JOIN auth_user a ON c.user_id = a.id
+JOIN bangazonapi_customer d ON d.id = f.seller_id
+JOIN auth_user b ON d.user_id = b.id
