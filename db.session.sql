04682c8b6065d924dbc3e5a5de88fe3622babb8c
Dan Ross <daniellross@gmail.com>
2021-06-04 13:13:29 -0500

    group_by was not workig with alias, referenced order_id directly

--- db.session.sql
+++ db.session.sql
@@ -5,10 +5,10 @@ SELECT
     pay.merchant_name as payment_card_type,
     SUM(p.price) as order_total
 FROM bangazonapi_order b_o
-LEFT JOIN bangazonapi_customer c ON c.id = b_o.customer_id
-LEFT JOIN auth_user a ON a.id = c.user_id
-LEFT JOIN bangazonapi_payment pay ON pay.id = b_o.payment_type_id 
+INNER JOIN bangazonapi_customer c ON c.id = b_o.customer_id
+INNER JOIN auth_user a ON a.id = c.user_id
+INNER JOIN bangazonapi_payment pay ON pay.id = b_o.payment_type_id 
 LEFT JOIN bangazonapi_orderproduct op ON op.order_id = b_o.id
 LEFT JOIN bangazonapi_product p ON p.id = op.product_id
 WHERE b_o.payment_type_id IS NOT NULL
-GROUP BY order_id
+GROUP BY b_o.id
